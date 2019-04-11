7da373c50ac290aceb1758330b275ab9ab641e36
Leonardo Risch <leo.mrisch@gmail.com>
2019-04-11 10:46:27 -0300

    add controller spec to check available movements

--- backend/test/KnightController.test.js
+++ backend/test/KnightController.test.js
@@ -17,13 +17,22 @@ afterAll(function(done){
 describe('post knight position', () => {
  test('when data is valid', async (done) => {
    const params = {
-     position: 'D1'
+     position: 'A1'
    }
+   const expectedFirstMovements = [{"x":3,"y":2,"position":"C2"},{"x":2,"y":3,"position":"B3"}];
+   const expectedSecondMovements = [
+     {"x":5,"y":1,"position":"E1"},{"x":5,"y":3,"position":"E3"},{"x":1,"y":1,"position":"A1"},{"x":1,"y":3,"position":"A3"},
+     {"x":4,"y":4,"position":"D4"},{"x":2,"y":4,"position":"B4"},{"x":4,"y":2,"position":"D2"},
+     {"x":4,"y":4,"position":"D4"},{"x":3,"y":1,"position":"C1"},{"x":3,"y":5,"position":"C5"},
+     {"x":1,"y":1,"position":"A1"},{"x":1,"y":5,"position":"A5"}
+   ]
+
    request.post(`/knight`)
           .send(params)
-          .expect(200)
-          .end(done);
-
+          .then(response => {
+            expect(JSON.parse(response.text)).toEqual({"firstMovements": expectedFirstMovements, "secondMovements": expectedSecondMovements})
+            done();
+          })
  });
 
  test('when data is invalid', async (done) => {
